a17e3d3650d30d14dc091f59b6490b0956516ce7
Darrow <nicedarrow12@gmail.com>
2022-01-11 16:23:04 -0300

    Primeiro componente React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
+  return <RepositoryList />
+}
+
+/* 
+
+! Geralmente, só essa estrutura já é um componente react:
+  export function App() {
   return <h1>hello world</h1>
 }
+
+  * Começar um componente com letra maiúscula;
+  * Um componente por arquivo;
+  * 
+
+*/
