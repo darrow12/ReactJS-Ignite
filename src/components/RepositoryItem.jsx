c3426217b70840c438eab651af0742114688a775
Darrow <nicedarrow12@gmail.com>
2022-01-11 17:07:27 -0300

    Aprofundando o conceito de propriedades no React

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,11 +2,12 @@
 export function RepositoryItem(props) {
   return (
     <li>
-    <strong>{props.repository ?? 'Default'}</strong>
+    <strong>{props.repository.name ?? 'Default'}</strong>
     {/* Caso o repositório esteja vazio, coloque um nome 'default' para todos que não tem | isso desconsidera que o 0 é um valor inválido | o sinal || (ou) também funciona */}
-    <p>Forms in React</p>
+    {/* Caso não queira fazer daquele jeito de colocar o repository nos 4, é só colocar aqui: props.repository?.name ?? 'Default' | Ele vai verificar se o repository tá nulo ou nao, caso estiver, ele nem procura pelo nome e retorna logo o default*/}
+    <p>{props.repository.description}</p>
 
-    <a href="">
+    <a href={props.repository.link}>
       Acessar repositório
     </a>
   </li>
@@ -14,3 +15,7 @@ export function RepositoryItem(props) {
 }
 
 // Sempre que quero retornar mais de uma linha de html, eu coloco parenteses. Se for uma linha, mantém nela mesmo
+
+
+
+// Resumo do conceito de propriedades: a forma de conseguir passar informações de um componente pai/mãe pros componentes filhos
