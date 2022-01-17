a96ad24da96344e38555bc51105642de07eb3cf8
Darrow <nicedarrow12@gmail.com>
2022-01-17 20:47:49 -0300

    Estilizando a listagem de repositórios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
+import './styles/repositories.scss'
 
 export function App() {
   return (
