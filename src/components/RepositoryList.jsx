23dc16672672770a2a5e7b1cdbcb6ad3fb2ba980
Darrow <nicedarrow12@gmail.com>
2022-01-17 21:18:26 -0300

    Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+
+// https://api.github.com/users/darrow12/repos
 
 const repository = {
   name: 'unform',
@@ -7,6 +11,17 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/darrow12/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+  // Cuidado pra não deixar o segundo parametro, porque ele vai ficar em loop
+
+  // console.log(repositories)
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
